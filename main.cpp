3458392651fa364c28844743232bcbfbc78ae98a
Yuriynv <164543230+Yuriynv@users.noreply.github.com>
2024-04-29 12:55:25 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,47 +1,134 @@
 #include <iostream>
+#include <locale.h>
+#include <string.h>
 #include <stdlib.h>
 
-float calcular_nota(float custo, float lucro, float imposto) {
-	float valor = custo + lucro;
-	float imposto_rs = valor * imposto / 100;
+void hangman_game(char level){
+	switch (level){	
+		case 1:
+			printf("\n\n\n");	
+			printf("\t\t\t  ,,, \n");
+			printf("\t\t\t (º º)\n");
+			printf("\t\t\t _'''_ \n");
+			printf("\t\t\t   | \n");
+			printf("\t\t\t _/ \\_ \n");
+			printf("=====================================\n");
+			break;
+		case 2:
+			printf("\n\n");	
+			printf("\t\t\t  ,,, \n");
+			printf("\t\t\t (º º)\n");
+			printf("\t\t\t _'''_ \n");
+			printf("\t\t\t   | \n");
+			printf("\t\t\t  / \\  \n");
+			printf("=====================================\n");
+			break;
+		case 3:
+			printf("\n\n");	
+			printf("\t\t\t  ,,, \n");
+			printf("\t\t\t (º º)\n");
+			printf("\t\t\t _'''_ \n");
+			printf("\t\t\t   | \n");
+			printf("\t\t\t       \n");
+			printf("=====================================\n");
+			break;
+		case 4:
+			printf("\n\n");	
+			printf("\t\t\t  ,,, \n");
+			printf("\t\t\t (º º)\n");
+			printf("\t\t\t _'''_ \n");
+			printf("\t\t\t     \n");
+			printf("\t\t\t       \n");
+			printf("=====================================\n");
+			break;
+		case 5:
+			printf("\n\n");	
+			printf("\t\t\t  ,,, \n");
+			printf("\t\t\t (º º)\n");
+			printf("\t\t\t  '''   \n");
+			printf("\t\t\t     \n");
+			printf("\t\t\t       \n");
+			printf("=====================================\n");
+			break;
+		case 6:
+			printf("\n\n");	
+			printf("\t\t\t  ,,, \n");
+			printf("\tMORREU, OTÁRIO!\t (x x)\n");
+			printf("\t\t\t  '''   \n");
+			printf("\t\t\t     \n");
+			printf("\t\t\t       \n");
+			printf("=====================================\n");
+			break;
+	}
+}
+
+void apresentar(int tamanho, char letras[], char letras_usadas[]) {
 	
-	float valor_final = (valor + imposto_rs) / ((100 - imposto) / 100);
+	printf("\n\tLetras Usadas: [");
+	for (int i = 0; i < strlen(letras_usadas); i++) {
+		printf("%c", letras_usadas[i]);
+		if (i < (strlen(letras_usadas) -1)) {
+			printf(", ");
+		}
+	}
+	printf("]\n");
 	
-	return valor_final;
+	printf("\n\t");
+	for (int i = 0; i < tamanho; i++) {
+		printf("%c", letras[i]);
+		printf(" ");
+	}
 }
 
 int main(int argc, char** argv) {
+	setlocale(LC_ALL, "Portuguese");
 	
-	float custo, lucro, imposto, valor_final;
+	int tamanho = 46, hit, vazio;
+	char palavra[tamanho];
+	char letras[strlen(palavra)];
+	char letra;
 	
-	do {
-		printf("\nDigite o custo: R$");
-		scanf("%f", &custo);
-	} while (custo < 0);
-	
-	do {
-		system("cls");
-		printf("\nCusto Objtido: R$ %.2f", custo);
-		printf("\nDigite o lucro desejado: R$");
-		scanf("%f", &lucro);
-	} while (lucro < 0);
-	
-	do {
-		system("cls");
-		printf("\nCusto Obtido: R$ %.2f", custo);
-		printf("\nLucro Desejado: R$ %.2f", lucro);
-		printf("\nDigite o total de imposto em %: ");
-		scanf("%f", &imposto);
-	} while (imposto < 0);
-	
+	printf("\n\tDigite a palavra desafio: ");
+	scanf("%s", &palavra);
+	tamanho = strlen(palavra);
 	system("cls");
-	printf("\nCusto Obtido: R$ %.2f", custo);
-	printf("\nLucro Desejado: R$ %.2f", lucro);
-	printf("\nTotal de Imposto em %: ");
 	
-	valor_final = calcular_nota(custo, lucro, imposto);
+	for (int i = 0; i < tamanho; i++) {
+		letras[i] = '_';
+	}
 	
-	printf("\nValor a cobrar: R$ %.2f", valor_final);
+	int level = 1;
+	char *letras_usadas = NULL;
+	letras_usadas = (char*)malloc(sizeof(char));
 	
-	return 0;
+	while (level < 6) {
+		hit = 0;
+		
+		hangman_game(level);
+		
+		printf("\n\t");
+		std::cin.ignore();
+		
+		apresentar(tamanho, letras, letras_usadas);
+		
+		printf("\n\n\tDigite uma letra: ");
+		scanf("%c", &letra);
+		
+		letras_usadas[strlen(letras_usadas)] = letra;
+		letras_usadas = (char*)realloc((char*)letras_usadas, strlen(letras_usadas) + sizeof(char));
+		
+		for (int i = 0; i < tamanho; i++) {
+			if (letra == palavra[i]) {
+				letras[i] = letra;
+				hit += 1;
+			}
+		}
+		
+		if (hit == 0) level += 1;
+		
+		system("cls");
+	}
+	hangman_game(level);
+	
+	printf("\n\tDigite qualquer tecla para encerrar.\n");
 }
